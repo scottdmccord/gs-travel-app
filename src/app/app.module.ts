c108abb890953571ab5fdf2efb1aeeb5a087ac87
Scott McCord <scott.sdm27@gmail.com>
2020-04-13 14:33:11 +0200

    Add new components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,29 @@ import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { PinMapComponent } from './pin-map/pin-map.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CitiesListComponent } from './cities-list/cities-list.component';
+import { RatingsComponent } from './ratings/ratings.component';
+import { RouterModule } from '@angular/router';
+
+const appRoutes = [
+  { path: 'map', component: PinMapComponent },
+  { path: 'cities', component: CitiesListComponent },
+  { path: 'ratings', component: RatingsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     PinMapComponent,
-    NavBarComponent
+    NavBarComponent,
+    CitiesListComponent,
+    RatingsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
