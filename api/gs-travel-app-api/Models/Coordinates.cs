4700e397738d1bdd6fb0ea06ed9dcc8bd33e941d
Scott McCord <scott.sdm27@gmail.com>
2021-04-03 09:29:11 +0200

    Changed coordinates to struct

--- api/gs-travel-app-api/Models/Coordinates.cs
+++ api/gs-travel-app-api/Models/Coordinates.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace gs_travel_app_api.Models
 {
-  public class Coordinates
+  public struct Coordinates
   {
     [Required]
     public double Latitude { get; set; }
