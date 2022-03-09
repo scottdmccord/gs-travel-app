0403ac67475dcaa16687965f97e1c8087e23087e
Scott McCord <scott.sdm27@gmail.com>
2022-03-09 14:09:45 +0100

    Added Swagger

--- api/gs-travel-app-api/Startup.cs
+++ api/gs-travel-app-api/Startup.cs
@@ -1,8 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +25,8 @@ namespace gs_travel_app_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen();
+
             services.AddDbContextPool<MariaDbContext>(
                 dbContextOptions => dbContextOptions
                     .UseMySql(
@@ -54,6 +54,8 @@ namespace gs_travel_app_api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
